675f5842167ba173918ce64ce1cd01eff2d7db74
Glansible <obraztsovgleb@gmail.com>
2021-12-26 14:43:50 +0300

    rename file

--- lesson_2.py
+++ lesson_2.py
@@ -1,3 +1,5 @@
+# Rebrain_Python lesson 2
+
 first_string = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
 first_string_list = first_string.split(' ')
 var11 = ' '.join(first_string_list[:3])
@@ -8,15 +10,13 @@ print(f"1.2 {first_string_list[4].removesuffix(':')}")
 print(f"1.3 {first_string.replace('ideapad', 'PC-12092')}")
 print(f"1.4 {first_string.rfind('failed')}")
 print(f"1.5 {first_string.lower().count('s')}")
-print(f"1.6 не могу догадаться как это сделать без цикла и фукнции sum")
+print(f"1.6 {int(var16[0]) + int(var16[1]) + int(var16[2])}")
 
 second_string = 'May 24 14:03:01 ideapad colord[844]: failed to get session [pid 8279]: Нет доступных данных'
 second_string_list = second_string.split(' ')
 pc_name = second_string_list[3]
 svc_name = second_string_list[4].removesuffix(':')
 log_date = ' '.join(second_string_list[:3])
-#log_mess = ' '.join(second_string_list[5:9])
-#err_mess = ' '.join(err_mess)
 
 another_second_string_list = second_string.split(': ')
 log_mess = another_second_string_list[-2]
