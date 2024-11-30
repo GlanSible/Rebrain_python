e2080cda8be026b793be5cffececda0f97758ce3
Луций Юний <obraztsovgleb@gmail.com>
2024-11-30 09:16:06 +0300

    remove task commit, flake8 fixes

--- final_assign/final.py
+++ final_assign/final.py
@@ -1,8 +1,6 @@
 import psutil
 import requests
-import socket
 import json
-import os
 import platform
 import logging
 
@@ -14,13 +12,17 @@ host_full_info = {}
 
 logging.info("Start to gather info about system:")
 
+
 def host_information():
     logging.info("Gathering Host information:")
     host_information = {}
     host_information['name'] = platform.node()
     host_information['platform'] = platform.system()
     host_full_info['host_information'] = host_information
+
+
 host_information()
+
 
 def network_info():
     logging.info("Gathering Network information:")
@@ -36,7 +38,10 @@ def network_info():
         else:
             pass
         host_full_info['network'] = [iface_dict, ip_dict]
+
+
 network_info()
+
 
 def disk():
     logging.info("Gathering Disk information:")
@@ -45,41 +50,48 @@ def disk():
         if not str(sdiskpart.device).startswith('/dev/loop'):
             disk_list.append(sdiskpart._asdict())
             host_full_info['disk'] = disk_list
+
+
 disk()
+
 
 def memory():
     logging.info("Gathering RAM information:")
     # need to switch values to Mb
     host_full_info['memory'] = psutil.virtual_memory()._asdict()
+
+
 memory()
+
 
 def cpu():
     logging.info("Gathering CPU information:")
-    cpu ={}
+    cpu = {}
     cpu['cpu_cores'] = psutil.cpu_count()
     cpu['cpu_physical_cores'] = psutil.cpu_count(logical=False)
     cpu['cpu_freqency'] = psutil.cpu_freq()
     host_full_info['cpu'] = cpu
+
+
 cpu()
+
 
 def load_average():
     logging.info("Gathering LA information:")
     load_average = dict(zip(['1 min', '5 min', '15 min'], psutil.getloadavg()))
     host_full_info['load_average'] = load_average
+
+
 load_average()
 
-print(host_full_info)
 
-# r = requests.post('http://127.0.0.1:8000/api/servers/add', json=simple_psutil())
+jsoned_host_full_info = json.dumps(host_full_info, indent=True)
+logging.info(f"""Gathered info {host_full_info['host_information']['name']} server:
+{jsoned_host_full_info}""")
 
-# print(f"Status Code: {r.status_code}, Response: {r.json()}")
+loaded_host_full_info = json.loads(jsoned_host_full_info)
 
-'''
-Программа должна обрабатывать данные о системе, полученные от утилиты psutil, и отправлять их на сервер с интервалом в 1 минуту в формате:
-{'host_information': {'sysname': ..., 'hostname' : ...},
- 'network': [{'interface': up/down, 'mtu': ... }...],
- 'disk': [{'disk: ..., 'mountpoint': ..., 'file_system_type', 'total': ..., 'used': ....} ],
- 'memory': {'memory_total': ..., 'memory_used': ..., 'memory_percent': ...},
- 'cpu': {'cpu_cores': ..., 'cpu_physical_cores': ..., 'cpu_freqency': {...}},
- 'load_average': {'1 min': ..., '5 min': ..., '15 min': ...}}}
-'''
+r = requests.post('http://127.0.0.1:8000/api/servers/add',
+                  json=loaded_host_full_info)
+
+print(f"Status Code: {r.status_code}, Response: {r.json()}")
