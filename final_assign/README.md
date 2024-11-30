88a0ec593da576ba57666f9719af288a0cc1727c
Луций Юний <obraztsovgleb@gmail.com>
2024-11-30 20:29:53 +0300

    add REAMDE, switched names for script and main func

---
+++ final_assign/README.md
@@ -0,0 +1,9 @@
+#### Small description
+Here you will find a final task script for Rebrain Python task described below:
+https://my.rebrainme.com/course/python/task/298  
+P.S. It also uses lesson_14 Django API for storing collected data.
+
+### How to use this script
+1) create Python venv with 'python3 -m venv <myenvname>' command
+2) execute source command
+3) run python3 final.py
--- final_assign/host_extended_info.py
+++ final_assign/host_extended_info.py
@@ -8,9 +8,9 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def host_full_info():
-    # Define host_full_info dict as data structure
-    host_full_info = {}
+def host_extended_info():
+    # Define host_extended_info dict as data structure
+    host_extended_info = {}
 
     logging.info("Start to gather info about system:")
 
@@ -18,7 +18,7 @@ def host_full_info():
     host_information = {}
     host_information['name'] = platform.node()
     host_information['platform'] = platform.system()
-    host_full_info['host_information'] = host_information
+    host_extended_info['host_information'] = host_information
 
     logging.info("Gathering Network information:")
     iface_dict = {}
@@ -32,55 +32,55 @@ def host_full_info():
             ip_dict['public_address'] = str(requests.get('https://ifconfig.me/ip').text)
         else:
             pass
-        host_full_info['network'] = [iface_dict, ip_dict]
+        host_extended_info['network'] = [iface_dict, ip_dict]
 
     logging.info("Gathering Disk information:")
     disk_list = []
     for sdiskpart in psutil.disk_partitions():
         if not str(sdiskpart.device).startswith('/dev/loop'):
             disk_list.append(sdiskpart._asdict())
-            host_full_info['disk'] = disk_list
+            host_extended_info['disk'] = disk_list
 
     logging.info("Gathering RAM information:")
-    host_full_info['memory'] = psutil.virtual_memory()._asdict()
+    host_extended_info['memory'] = psutil.virtual_memory()._asdict()
     # Iterate over memory dict to transform bytes into MiB
-    for k, v in host_full_info['memory'].items():
+    for k, v in host_extended_info['memory'].items():
         if v <= 100:
             pass
         else:
             v = round(v / 1024 ** 2)
-            host_full_info['memory'][k] = v
+            host_extended_info['memory'][k] = v
 
     logging.info("Gathering CPU information:")
     cpu = {}
     cpu['cpu_cores'] = psutil.cpu_count()
     cpu['cpu_physical_cores'] = psutil.cpu_count(logical=False)
     cpu['cpu_freqency'] = psutil.cpu_freq()
-    host_full_info['cpu'] = cpu
+    host_extended_info['cpu'] = cpu
 
     logging.info("Gathering LA information:")
     load_average = dict(zip(['1 min', '5 min', '15 min'], psutil.getloadavg()))
-    host_full_info['load_average'] = load_average
+    host_extended_info['load_average'] = load_average
     # Round float values up to 2 signs
-    for k, v in host_full_info['load_average'].items():
+    for k, v in host_extended_info['load_average'].items():
         v = round(v, 2)
-        host_full_info['load_average'][k] = v
+        host_extended_info['load_average'][k] = v
 
     logging.info("Serializing gathered data.")
-    jsoned_host_full_info = json.dumps(host_full_info, indent=True)
+    jsoned_host_extended_info = json.dumps(host_extended_info, indent=True)
 
-    logging.info(f"""Gathered info about {host_full_info['host_information']['name']} server:
-                 {jsoned_host_full_info}""")
+    logging.info(f"""Gathered info about {host_extended_info['host_information']['name']} server:
+                 {jsoned_host_extended_info}""")
 
-    loaded_host_full_info = json.loads(jsoned_host_full_info)
+    loaded_host_extended_info = json.loads(jsoned_host_extended_info)
 
-    return loaded_host_full_info
+    return loaded_host_extended_info
 
 
 try:
     r = requests.post(
         'http://127.0.0.1:8000/api/servers/add-extended',
-        json=host_full_info())
+        json=host_extended_info())
 
     r.raise_for_status()
 
